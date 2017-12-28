37dc4e8bd92883cfe819ecb35d42933073205dcc
James Bryant <>
2017-12-27 22:27:03 -0500

    Add ability to search by file pattern.

--- ex7/grep.py
+++ ex7/grep.py
@@ -12,22 +12,23 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("search_pattern",
                         type=str, help="Pattern to find in file.")
-    parser.add_argument("file_pattern",
-                        type=str, help="File pattern to search within")
+    parser.add_argument("file_pattern", nargs='+',
+                        help="File pattern to search within")
     args = parser.parse_args()
-    print("search pattern: ", args.search_pattern)
-    print("file pattern: ", args.file_pattern)
+    # print("search pattern: ", args.search_pattern)
+    # print("file pattern: ", args.file_pattern)
     return args
 
 def run():
     args = parse_args()
 
     # Load File(s)
-    file = open(args.file_pattern, 'r')
-    for line in file:
-        line = line.strip('\n')
-        if re.search(args.search_pattern, line):
-            print("{}: {}".format(file.name, line))
+    for file in args.file_pattern:
+        file = open(file, 'r')
+        for line in file:
+            line = line.strip('\n')
+            if re.search(args.search_pattern, line):
+                print("{}: {}".format(file.name, line))
 
 # MAIN START
 
